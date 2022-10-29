9dd4d22e4e29e3f42a86dc8924d59b29504a2268
C. Felix Lee <felixlee.music@gmail.com>
2022-10-29 19:19:50 -0400

    GET route for reviews is working

---
+++ server/api/reviews.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express.Router()
+const { Review } = require('../db')
+
+module.exports = app
+
+app.get('/:id', async (req, res, next) => {
+    try {
+        const reviews = await Review.findAll({
+            where: { productId: req.params.id },
+        })
+        res.send(reviews)
+    } catch (ex) {
+        next(ex)
+    }
+})
--- server/db/index.js
+++ server/db/index.js
@@ -1,46 +1,66 @@
-const conn = require('./conn');
-const User = require('./User');
-const Product = require('./Product');
-const Order = require('./Order');
-const LineItem  = require('./LineItem');
+const conn = require('./conn')
+const User = require('./User')
+const Product = require('./Product')
+const Order = require('./Order')
+const LineItem = require('./LineItem')
+const Review = require('./Review')
 
-Order.belongsTo(User);
-LineItem.belongsTo(Order);
-Order.hasMany(LineItem);
-LineItem.belongsTo(Product);
+Order.belongsTo(User)
+LineItem.belongsTo(Order)
+Order.hasMany(LineItem)
+LineItem.belongsTo(Product)
+Review.belongsTo(Product)
+Product.hasMany(Review)
 
-const syncAndSeed = async()=> {
-  await conn.sync({ force: true });
-  const [moe, lucy, larry, foo, bar, bazz, ethyl] = await Promise.all([
-    User.create({ username: 'moe', password: '123' }),
-    User.create({ username: 'lucy', password: '123' }),
-    User.create({ username: 'larry', password: '123' }),
-    Product.create({ name: 'foo' }),
-    Product.create({ name: 'bar' }),
-    Product.create({ name: 'bazz' }),
-    User.create({ username: 'ethyl', password: '123' }),
-  ]);
-
-  const cart = await ethyl.getCart();
-  await ethyl.addToCart({ product: bazz, quantity: 3});
-  await ethyl.addToCart({ product: foo, quantity: 2});
-  return {
-    users: {
-      moe,
-      lucy,
-      larry
-    },
-    products: {
-      foo,
-      bar,
-      bazz
+const syncAndSeed = async () => {
+    await conn.sync({ force: true })
+    const [moe, lucy, larry, foo, bar, bazz, ethyl] = await Promise.all([
+        User.create({ username: 'moe', password: '123' }),
+        User.create({ username: 'lucy', password: '123' }),
+        User.create({ username: 'larry', password: '123' }),
+        Product.create({ name: 'foo' }),
+        Product.create({ name: 'bar' }),
+        Product.create({ name: 'bazz' }),
+        User.create({ username: 'ethyl', password: '123' }),
+    ])
+    const testReview = await Review.create({
+        productId: foo.id,
+        userId: lucy.id,
+        rating: '5',
+        text: 'what an awesome read! highly recommend :D',
+    })
+    const testReview2 = await Review.create({
+        productId: foo.id,
+        userId: lucy.id,
+        rating: '3',
+        text: 'kinda meh...',
+    })
+    const testReview3 = await Review.create({
+        productId: bar.id,
+        userId: lucy.id,
+        rating: '4',
+        text: 'pretty good!',
+    })
+    const cart = await ethyl.getCart()
+    await ethyl.addToCart({ product: bazz, quantity: 3 })
+    await ethyl.addToCart({ product: foo, quantity: 2 })
+    return {
+        users: {
+            moe,
+            lucy,
+            larry,
+        },
+        products: {
+            foo,
+            bar,
+            bazz,
+        },
     }
-  };
-};
-
+}
 
 module.exports = {
-  syncAndSeed,
-  User,
-  Product
-};
+    syncAndSeed,
+    User,
+    Product,
+    Review,
+}
