995e7b56697514cdaa5d9589bc527173c4a7c178
Kendal Enz <kendal.enz@gmail.com>
2022-10-30 00:38:24 -0400

    cleaned up code

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,5 @@
 const conn = require('./conn');
 const User = require('./User');
-// const Product = require('./Product');
 const Book = require('./Book');
 const Order = require('./Order');
 const LineItem  = require('./LineItem');
@@ -18,8 +17,6 @@ const syncAndSeed = async()=> {
     User.create({ username: 'larry', password: '123' }),
     Book.create({ title: 'Wayward', author: 'Chris Burkhard', genre: 'Non-fiction', description: 'Wayward is a collection of strking photographs and the revealing personal stories behind them by one of the leading surf, nature, and adventure photographers of our time.', price: '35.00'}),
     Book.create({ title: 'Girl in Landscape', author: 'Jonathan Lethem', genre: 'Literary Fiction', description: 'Girl in Landscape finds Lethem once again twisting forms and literary conventions to create a dazzling, completely unconventional tale that manages simultaneously to amaze and move the reader. The heronine is a fourteen-year-old Pella Marsh, whose mother dies just as her family flees a postapocalyptic Brooklyn for the frontier of a recently discovered planet.', price: '22.95'}),
-    // Product.create({ name: 'bar' }),
-    // Product.create({ name: 'bazz' }),
     User.create({ username: 'ethyl', password: '123' }),
   ]);
 
@@ -34,8 +31,7 @@ const syncAndSeed = async()=> {
     },
     books: {
       wayward,
-      girlInLandscape,
-      // bazz
+      girlInLandscape
     }
   };
 };
