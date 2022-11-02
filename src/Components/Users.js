11d7b0a6a8a2d7a982361f1704681893a955c45e
Anisah Leila <anisahleila@hotmail.com>
2022-11-02 01:07:37 +0000

    account info

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Users = () => {
-  return <hr />;
+  const { auth } = useSelector((state) => state);
+  return (
+    <div>
+      <h1>Account Information</h1>
+      <pre>{JSON.stringify(auth, null, 2)}</pre>
+    </div>
+  );
 };
 
 export default Users;
