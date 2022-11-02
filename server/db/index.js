b274baebb4c1e196e060691f4e6af80dd2ba71af
C. Felix Lee <felixlee.music@gmail.com>
2022-11-02 01:03:01 -0400

    commit

--- server/db/index.js
+++ server/db/index.js
@@ -14,9 +14,27 @@ const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const [moe, lucy, larry, wayward, girlInLandscape, ethyl] = await Promise.all(
     [
-      User.create({ username: 'moe', password: '123' }),
-      User.create({ username: 'lucy', password: '123' }),
-      User.create({ username: 'larry', password: '123' }),
+      User.create({
+        username: 'moe',
+        password: '123',
+        firstName: 'monique',
+        lastName: 'harvey',
+        email: 'moe@fullstack.edu',
+      }),
+      User.create({
+        username: 'lucy',
+        password: '123',
+        firstName: 'lucinda',
+        lastName: 'daniel',
+        email: 'lucy@fullstack.edu',
+      }),
+      User.create({
+        username: 'larry',
+        password: '123',
+        firstName: 'laurence',
+        lastName: 'fishburn',
+        email: 'larry@fullstack.edu',
+      }),
       Book.create({
         title: 'Wayward',
         author: 'Chris Burkhard',
@@ -28,102 +46,57 @@ const syncAndSeed = async () => {
       Book.create({
         title: 'Girl in Landscape',
         author: 'Jonathan Lethem',
-        genre: 'Fiction',
+        genre: 'Literary Fiction',
         description:
           'Girl in Landscape finds Lethem once again twisting forms and literary conventions to create a dazzling, completely unconventional tale that manages simultaneously to amaze and move the reader. The heronine is a fourteen-year-old Pella Marsh, whose mother dies just as her family flees a postapocalyptic Brooklyn for the frontier of a recently discovered planet.',
         price: '22.95',
       }),
-      User.create({ username: 'ethyl', password: '123' }),
+      User.create({
+        username: 'ethyl',
+        password: '123',
+        firstName: 'ethylin',
+        lastName: 'grace',
+        email: 'ethyl@fullstack.edu',
+      }),
     ]
   );
-
-  const syncAndSeed = async () => {
-    await conn.sync({ force: true });
-    const [moe, lucy, larry, wayward, girlInLandscape, ethyl] =
-      await Promise.all([
-        User.create({
-          username: 'moe',
-          password: '123',
-          firstName: 'monique',
-          lastName: 'harvey',
-          email: 'moe@fullstack.edu',
-        }),
-        User.create({
-          username: 'lucy',
-          password: '123',
-          firstName: 'lucinda',
-          lastName: 'daniel',
-          email: 'lucy@fullstack.edu',
-        }),
-        User.create({
-          username: 'larry',
-          password: '123',
-          firstName: 'laurence',
-          lastName: 'fishburn',
-          email: 'larry@fullstack.edu',
-        }),
-        Book.create({
-          title: 'Wayward',
-          author: 'Chris Burkhard',
-          genre: 'Non-fiction',
-          description:
-            'Wayward is a collection of strking photographs and the revealing personal stories behind them by one of the leading surf, nature, and adventure photographers of our time.',
-          price: '35.00',
-        }),
-        Book.create({
-          title: 'Girl in Landscape',
-          author: 'Jonathan Lethem',
-          genre: 'Literary Fiction',
-          description:
-            'Girl in Landscape finds Lethem once again twisting forms and literary conventions to create a dazzling, completely unconventional tale that manages simultaneously to amaze and move the reader. The heronine is a fourteen-year-old Pella Marsh, whose mother dies just as her family flees a postapocalyptic Brooklyn for the frontier of a recently discovered planet.',
-          price: '22.95',
-        }),
-        User.create({
-          username: 'ethyl',
-          password: '123',
-          firstName: 'ethylin',
-          lastName: 'grace',
-          email: 'ethyl@fullstack.edu',
-        }),
-      ]);
-    await Review.create({
-      userId: moe.id,
-      bookId: wayward.id,
-      rating: 5,
-      text: 'amaaaazing!!',
-    });
-    await Review.create({
-      userId: lucy.id,
-      bookId: wayward.id,
-      rating: 4,
-      text: 'must buy!!',
-    });
-    await Review.create({
-      userId: larry.id,
-      bookId: girlInLandscape.id,
-      rating: 5,
-      text: '2 thumbs up!!',
-    });
-    await Review.create({
-      userId: ethyl.id,
-      bookId: girlInLandscape.id,
-      rating: 5,
-      text: 'greeaaaat!!',
-    });
-    const cart = await ethyl.getCart();
-    await ethyl.addToCart({ book: wayward, quantity: 3 });
-    await ethyl.addToCart({ book: girlInLandscape, quantity: 2 });
-    return {
-      users: {
-        moe,
-        lucy,
-        larry,
-      },
-      books: {
-        wayward,
-        girlInLandscape,
-      },
-    };
+  await Review.create({
+    userId: moe.id,
+    bookId: wayward.id,
+    rating: 5,
+    text: 'amaaaazing!!',
+  });
+  await Review.create({
+    userId: lucy.id,
+    bookId: wayward.id,
+    rating: 4,
+    text: 'must buy!!',
+  });
+  await Review.create({
+    userId: larry.id,
+    bookId: girlInLandscape.id,
+    rating: 5,
+    text: '2 thumbs up!!',
+  });
+  await Review.create({
+    userId: ethyl.id,
+    bookId: girlInLandscape.id,
+    rating: 5,
+    text: 'greeaaaat!!',
+  });
+  const cart = await ethyl.getCart();
+  await ethyl.addToCart({ book: wayward, quantity: 3 });
+  await ethyl.addToCart({ book: girlInLandscape, quantity: 2 });
+  return {
+    users: {
+      moe,
+      lucy,
+      larry,
+    },
+    books: {
+      wayward,
+      girlInLandscape,
+    },
   };
 };
 
