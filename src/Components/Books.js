7e888b2f24927d10c0f13b45ae38ee2c6aa0deb8
Kendal Enz <kendal.enz@gmail.com>
2022-11-02 15:05:30 -0400

    flex box successes!

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -4,28 +4,49 @@ import { Link } from 'react-router-dom';
 
 const Books = () => {
   const { books } = useSelector((state) => state);
-  return (
-    <div>
 
-      {books.map((book) => {
+  const Bookcard = (props) => {
+    return (
+        <div style={{padding: '2rem', margin: '1rem'}}>
+            <img src={props.imageUrl} className='books_page_img' alt='Book cover'/>
+            <br></br>
+            <strong>{props.title}</strong>
+            <br></br>
+            <strong>{props.author}</strong>
+            <br></br>
+            {props.price}
+        </div>
+    );
+};
+
+  return (
+    <div className='books_div'>
+
+      {
+        books.map((book) => <Bookcard imageUrl={book.imageUrl} title={book.title} author={book.author} price={book.price}/>)
+        // {
         // const fictionGenre = books.filter(book => book.genre === 'Fiction');
 
         // const nonFictionGenre = books.filter(book => book.genre === 'Non-fiction');
-        return (
-          <div className='books_div'key={book.id}>
-            <img src={book.imageUrl} className='books_page_img' alt='Book cover'/>
-            <br></br>
-            <Link to={`/books/${book.id}`}><strong>{book.title}</strong></Link>
-            <br></br>
-            <strong>{book.author}</strong>
-            <br></br>
-            <br></br>
-            ${book.price}
-            <br></br>
-            <br></br>
-          </div>
-        );
-      })}
+            // return (
+            //     <>
+            //         <bookCard />
+            //     </>
+        //   <div className='books_div'key={book.id}>
+        //     <img src={book.imageUrl} className='books_page_img' alt='Book cover'/>
+        //     <br></br>
+        //     <Link to={`/books/${book.id}`}><strong>{book.title}</strong></Link>
+        //     <br></br>
+        //     <strong>{book.author}</strong>
+        //     <br></br>
+        //     <br></br>
+        //     ${book.price}
+        //     <br></br>
+        //     <br></br>
+        //   </div>
+            // );
+    //   })
+    }
     </div>
   );
 };
