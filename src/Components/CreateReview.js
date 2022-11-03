7fb45e835d04500c9eeea2c9aefe58536ab34718
C. Felix Lee <felixlee.music@gmail.com>
2022-11-03 15:54:07 -0400

    getting started on creating a review

--- src/Components/CreateReview.js
+++ src/Components/CreateReview.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 const createReview = () => {
+  const dispatch = useDispatch();
+  const { reviews } = useSelector((state) => state);
   return <hr />;
 };
 
