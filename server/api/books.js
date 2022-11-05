90d45cdb86f4df4f3a800f5fa291f7a0afac29c1
Kendal Enz <kendal.enz@gmail.com>
2022-11-05 14:13:04 -0400

    genre filter improvements

--- server/api/books.js
+++ server/api/books.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express.Router();
 const { Book } = require('../db');
+const conn = require('../db/conn');
 
 app.get('/', async (req, res, next) => {
   try {
-    const books = await Book.findAll();
+    const books = await Book.findAll({
+      order: conn.col('title')
+    })
     res.send(books);
   } catch (err) {
     next(err);
