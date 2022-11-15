abb344ac0a527b9ab84dfc618864ebde1a40d95c
C. Felix Lee <felixlee.music@gmail.com>
2022-11-15 15:55:31 -0500

    seeded more book data

--- server/db/index.js
+++ server/db/index.js
@@ -317,6 +317,16 @@ const syncAndSeed = async () => {
     imageUrl:
       'https://d2p7wwv96gt4xt.cloudfront.net/G/773149A4/EAN-9780061350962',
   });
+  await Book.create({
+    title: 'Parable of the Sower (Earthseed #1)',
+    author: 'Octavia E. Butler',
+    genre: 'Fiction',
+    description:
+      "When global climate change and economic crises lead to social chaos in the early 2020s, California becomes full of dangers, from pervasive water shortage to masses of vagabonds who will do anything to live to see another day. Fifteen-year-old Lauren Olamina lives inside a gated community with her preacher father, family, and neighbors, sheltered from the surrounding anarchy. In a society where any vulnerability is a risk, she suffers from hyperempathy, a debilitating sensitivity to others' emotions.    Precocious and clear-eyed, Lauren must make her voice heard in order to protect her loved ones from the imminent disasters her small community stubbornly ignores. But what begins as a fight for survival soon leads to something much more: the birth of a new faith . . . and a startling vision of human destiny. ",
+    price: '16.99',
+    imageUrl:
+      'https://d2p7wwv96gt4xt.cloudfront.net/G/363FFD5A/EAN-9781538732182',
+  });
   await Review.create({
     userId: cece.id,
     bookId: runner.id,
