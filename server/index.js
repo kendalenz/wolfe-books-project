e357a79e47013327a9f6a1ecfadfd299d99dba8e
Kendal Enz <kendal.enz@gmail.com>
2023-02-16 17:44:49 -0500

    addded start script

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const { syncAndSeed } = require('./db');
 const init = async () => {
   try {
     await syncAndSeed();
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3002;
     app.listen(port, () => console.log(`listening on port ${port}`));
   } catch (ex) {
     console.log(ex);
